74ff2a4cd3eb4b442d9e9214056bd223c9d2c39e
Gustavo <gustavooliveiracosta349@gmail.com>
2025-10-25 12:26:45 -0300

    Adicionando o UsuarioRepositorio no program.cs

--- FichasDePersonagens/Program.cs
+++ FichasDePersonagens/Program.cs
@@ -1,7 +1,11 @@
+using FichasDePersonagens.Repositorio;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<UsuarioRepositorio>();
 
 var app = builder.Build();
 
