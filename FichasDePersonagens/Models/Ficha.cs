502d944de90e5e588deca8ebab7fa4fd3f5e5650
Gustavo <gustavooliveiracosta349@gmail.com>
2025-10-25 11:31:57 -0300

    Criando a e configurando as models

---
+++ FichasDePersonagens/Models/Ficha.cs
@@ -0,0 +1,18 @@
+﻿namespace FichasDePersonagens.Models
+{
+    public class Ficha
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Origem { get; set; }
+        public string Classe { get; set; }
+        public int Forca { get; set; }
+        public int Agilidade { get; set; }
+        public int Inteligencia { get; set; }
+        public int Defesa { get; set; }
+        public int Energia { get; set; }
+        public string Habilidades { get; set; }
+        public string Curiosidades { get; set; }
+    }
+}
