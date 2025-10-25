8a5e0a2e82bb19b006c097b75b1e5463831901a7
Gustavo <gustavooliveiracosta349@gmail.com>
2025-10-25 12:24:48 -0300

    Criando e configurando as Controllers e adicionando as views

---
+++ FichasDePersonagens/Controllers/FichaController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FichasDePersonagens.Models;
+using FichasDePersonagens.Repositorio;
+
+namespace FichasDePersonagens.Controllers
+{
+    public class FichaController : Controller
+    {
+
+        private readonly FichaRepositorio _fichaRepositorio;
+
+        public FichaController(FichaRepositorio fichaRepositorio)
+        {
+            _fichaRepositorio = fichaRepositorio;
+        }
+
+        [HttpGet]
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Ficha ficha)
+        {
+            if (ModelState.IsValid)
+            {
+                _fichaRepositorio.AdicionarFicha(ficha);
+                return RedirectToAction("Index");
+            }
+            return View(ficha);
+        }
+    }
+}
---
+++ FichasDePersonagens/Controllers/UsuarioController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FichasDePersonagens.Models;
+using FichasDePersonagens.Repositorio;
+
+namespace FichasDePersonagens.Controllers
+{
+    public class UsuarioController : Controller
+    {
+
+        private readonly UsuarioRepositorio _usuarioRepositorio;
+
+        public UsuarioController(UsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string nome, string email, string senha)
+        {
+            var usuario = _usuarioRepositorio.ObterUsuario(email);
+
+            if (usuario != null && usuario.Senha == senha)
+            {
+                // Autenticação bem-sucedida
+                return RedirectToAction("Index", "Ficha");
+            }
+            ModelState.AddModelError("", "Email ou senha inválidos.");
+
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _usuarioRepositorio.AdicionarUsuario(usuario);
+                return RedirectToAction("Login");
+            }
+            return View(usuario);
+        }
+    }
+}
