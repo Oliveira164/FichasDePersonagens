be580180ba3a41b3425f418946ac20bac9794b81
Gustavo <gustavooliveiracosta349@gmail.com>
2025-10-25 11:06:35 -0300

    Criando o Projeto FichasDePersonagens

---
+++ FichasDePersonagens/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FichasDePersonagens.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
